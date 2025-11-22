85d4945fadbe2a61d2566f7b7a00e31f0cd76cc3
VirajGandhi07 <viraj.gandhi@bp.com>
2025-11-22 19:04:40 +0530

    Added PlayerManager backend

---
+++ DataManager/PlayerManager/PlayerManager/PlayerService.cs
@@ -0,0 +1,42 @@
+using System.Text.Json;
+using PlayerManagerApp.Models;
+
+namespace PlayerManagerApp.Services
+{
+    public class PlayerService
+    {
+        private const string FileName = "players.json";
+        public List<Player> Players { get; private set; } = new List<Player>();
+
+        public PlayerService()
+        {
+            LoadPlayers();
+        }
+
+        public void AddPlayer(string name)
+        {
+            Players.Add(new Player { Name = name });
+            SavePlayers();
+        }
+
+        public void SavePlayers()
+        {
+            var json = JsonSerializer.Serialize(Players, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FileName, json);
+        }
+
+        public void LoadPlayers()
+        {
+            if (File.Exists(FileName))
+            {
+                var json = File.ReadAllText(FileName);
+                Players = JsonSerializer.Deserialize<List<Player>>(json) ?? new List<Player>();
+            }
+        }
+
+        public List<Player> GetTopScores()
+        {
+            return Players.OrderByDescending(p => p.HighScore).Take(5).ToList();
+        }
+    }
+}
